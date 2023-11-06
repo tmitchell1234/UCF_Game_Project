ad1af2d37082ce9c4a4ea2e767e59ff47f1a14b0
tmitchell1234 <mitchelltom1234@gmail.com>
2023-11-05 20:06:32 -0500

    Added basic green alien and updated main menu

--- Assets/MainMenuStuff/Scripts/MainMenu.cs
+++ Assets/MainMenuStuff/Scripts/MainMenu.cs
@@ -6,20 +6,35 @@ using UnityEngine.SceneManagement; // required to load different scenes
 public class MainMenu : MonoBehaviour
 {
     public string firstLevel;
+
+    public GameObject optionsScreen;
+
+    public GameObject playButton;
+    public GameObject optionsButton;
+    public GameObject exitButton;
+
     public void StartGame()
     {
         // TODO: Uncomment this when ready to implement game control flow
         // SceneManager.LoadScene(firstLevel);
     }
 
-    public void OpenSettings()
+    public void OpenOptions()
     {
+        optionsScreen.SetActive(true);
 
+        playButton.SetActive(false);
+        optionsButton.SetActive(false);
+        exitButton.SetActive(false);
     }
 
     public void CloseOptions()
     {
+        optionsScreen.SetActive(false);
 
+        playButton.SetActive(true);
+        optionsButton.SetActive(true);
+        exitButton.SetActive(true);
     }
 
     public void ExitGame()
